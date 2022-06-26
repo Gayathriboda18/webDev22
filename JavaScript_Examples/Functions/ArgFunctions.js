3aec40ae1aba2c53077fe6fcbd136bddbd0dd0c5
Gayathriboda18 <bodagayathri18@outlook.com>
2022-06-26 20:07:57 +0530

    - exercises by colt

--- JavaScript_Examples/Functions/ArgFunctions.js
+++ JavaScript_Examples/Functions/ArgFunctions.js
@@ -31,7 +31,7 @@ bTech('1', 'CSSE');
 // -------------------Example 2 -------------------
 function num(s1, times) {
     result = ' ';
-    for (let i = 0; i < times; i++){
+    for (let i = 0; i < times; i++) {
         result += s1;
     }
     console.log(result);
@@ -40,16 +40,50 @@ num("Print me 3times", 4);
 
 //topic:--------------------  using "RETURN" in Functions
 
-//e.g. multiply 
-function multiply(x,y){
-    return x*y;
-}
+// //e.g. multiply
+// function multiply(x, y) {
+//     return x * y;
+// }
 
+//todo 
 //e.g. Return last element of Array
 
-function lastElement(ar){
-    if (ar.length() == '0'){
+function lastElement(ar) {
+    if (ar.length() == '0') {
         return null;
     }
     return ar.splice(-1).pop()
+}
+
+// //e.g. Return 1st letter of string Capitalize
+
+// function capitalize(word){
+//     let first = word.charAt(0 ).toUpperCase();
+//     let rest=word.slice(1);
+//     return first+rest
+// }
+
+// //e.g. Sum of an Array
+// function sumArray(array) {
+//   let sum = 0;
+
+//   for (let i = 0; i < array.length; i += 1) {
+//     sum += array[i];
+//   }
+  
+//   return sum;
+// }
+
+// sumArray([2, 4, 56, 6]);
+
+//todo : Solve this problem
+
+function returnDay(number){
+    const daysArray= {1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday',7:'Sunday'};
+    if(number<1 || number >7){
+        console.log("null");
+    }
+    else{
+     return daysArray[number];   
+    }
 }
